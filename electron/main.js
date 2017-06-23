d07f1100b33cb4a02b7440bdce12186d8b2e38bf
jmgeffroy <jmg@mantano.com>
2017-06-23 16:58:01 +0200

    Now displaying the Kotlin Tinkter app in the Electron app window...

--- electron/main.js
+++ electron/main.js
@@ -11,14 +11,15 @@ function createWindow () {
   win = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+  win.loadURL("http://localhost:8080")
+//url.format({
+//    pathname: path.join(__dirname, 'index.html'),
+//    protocol: 'file:',
+//    slashes: true
+//  }))
 
-  // Open the DevTools.
-  win.webContents.openDevTools()
+//  // Open the DevTools.
+//  win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
